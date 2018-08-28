b2441a9d4fd33918738676a9dc82b888eb273d11
Yayan Heryanto <yayanheryanto27@gmail.com>
2018-08-28 15:35:46 +0700

    layout add motor

--- app/src/main/java/com/yayanheryanto/larismotor/adapter/MotorAdapter.java
+++ app/src/main/java/com/yayanheryanto/larismotor/adapter/MotorAdapter.java
@@ -1,6 +1,8 @@
 package com.yayanheryanto.larismotor.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.yayanheryanto.larismotor.R;
@@ -54,7 +57,7 @@ public class MotorAdapter extends RecyclerView.Adapter<MotorAdapter.MotorViewHol
 
     public class MotorViewHolder extends RecyclerView.ViewHolder {
 
-        private ImageView imageMotor;
+        private ImageView imageMotor, imgDeelete;
         private TextView textNopol, textHjm;
         private View view;
 
@@ -63,6 +66,7 @@ public class MotorAdapter extends RecyclerView.Adapter<MotorAdapter.MotorViewHol
 
             this.view = itemView;
             imageMotor = itemView.findViewById(R.id.image);
+            imgDeelete = itemView.findViewById(R.id.imgDelete);
             textNopol = itemView.findViewById(R.id.txt_plat);
             textHjm = itemView.findViewById(R.id.txt_hjm);
         }
